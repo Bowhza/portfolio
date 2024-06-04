92d5834dcf56f94cd6d57cbe52c03bc5bb8f9fb6
Bowhza <bogtank2003@gmail.com>
2024-06-04 13:08:12 -0600

    Add style to nav links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,16 +4,16 @@ export default function Navigation() {
   return (
     <nav>
       <ul className="flex flex-col gap-3 my-5 font-semibold text-neutral-200 drop-shadow-lg">
-        <li className="text-lg nav-link">
+        <li className="text-lg nav-link hover:text-xl hover:text-teal-300 transition-all duration-300 ease-in-out">
           <a href="#about">About</a>
         </li>
-        <li className="text-lg nav-link">
+        <li className="text-lg nav-link hover:text-xl hover:text-teal-300 transition-all duration-300 ease-in-out">
           <a href="#education">Education</a>
         </li>
-        <li className="text-lg nav-link">
+        <li className="text-lg nav-link hover:text-xl hover:text-teal-300 transition-all duration-300 ease-in-out">
           <a href="#skills">Skills</a>
         </li>
-        <li className="text-lg nav-link">
+        <li className="text-lg nav-link hover:text-xl hover:text-teal-300 transition-all duration-300 ease-in-out">
           <a href="#projects">Projects</a>
         </li>
       </ul>
